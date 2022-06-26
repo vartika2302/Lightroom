868fa9cbb0c0d82c4acdedb055f1deeda60fee47
Vartika <guptavartika624@gmail.com>
2022-06-27 02:56:02 +0530

    Add role login component

--- lightroom-frontend/src/components/RoleLogin.jsx
+++ lightroom-frontend/src/components/RoleLogin.jsx
@@ -1,24 +1,72 @@
 import styled from "styled-components";
 
-const RoleLoginContainer = styled.div``;
+const RoleLoginContainer = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+`;
 
-const RoleLoginWrapper = styled.div``;
+const RoleLoginWrapper = styled.div`
+  width: 100%;
+`;
 
-const RoleLoginTop = styled.div``;
+const RoleLoginTop = styled.div`
+  display: flex;
+`;
 
-const RoleLoginBottom = styled.div``;
+const RoleLoginBottom = styled.div`
+  display: flex;
+`;
 
-const RoleLoginLeft = styled.div``;
+const RoleLoginLeft = styled.div`
+  flex: 1;
+`;
 
-const RoleLoginRight = styled.div``;
+const RoleLoginRight = styled.div`
+  flex: 1;
+`;
 
-const RoleLoginImage = styled.img``;
+const RoleRightWrapper = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  height: 100%;
+  margin-left: 7rem;
+`;
 
-const RoleLoginHeading = styled.h1``;
+const RoleLoginImage = styled.img`
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
+  border-radius: 61% 39% 65% 35% / 0% 100% 0% 100%;
+`;
 
-const RoleLoginInfo = styled.p``;
+const RoleLoginHeading = styled.h1`
+  font-family: "Poppins", sans-serif;
+  text-transform: uppercase;
+  width: 100%;
+`;
 
-const RoleLoginBtn = styled.button``;
+const RoleLoginHead = styled.h3`
+  color: #76ba99;
+`;
+
+const RoleLoginInfo = styled.p`
+font-size: 15px;
+font-family: 'Poppins',sans-serif;
+`;
+
+const RoleLoginBtn = styled.button`
+  background-color: #76ba99;
+  padding: 0.6rem 1.2rem;
+  border: none;
+  border-radius: 20px;
+  color: #fff;
+  font-size:0.9rem;
+  cursor: pointer;
+  text-transform: uppercase;
+`;
 
 function RoleLogin() {
   return (
@@ -26,29 +74,33 @@ function RoleLogin() {
       <RoleLoginWrapper>
         <RoleLoginTop>
           <RoleLoginLeft>
-            <RoleLoginImage src="https://st3.depositphotos.com/5383684/16706/v/380/depositphotos_167066338-stock-illustration-young-woman-teacher-in-front.jpg?forcejpeg=true" />
+            <RoleLoginImage src="https://images.hindi.news18.com/ibnkhabar/uploads/2022/05/pgt-1.jpg" />
           </RoleLoginLeft>
           <RoleLoginRight>
-            <RoleLoginHeading>
-              Are you a <span>TEACHER</span> ?
-            </RoleLoginHeading>
-            <RoleLoginInfo>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </RoleLoginInfo>
-            <RoleLoginBtn>Login</RoleLoginBtn>
+            <RoleRightWrapper>
+              <RoleLoginHeading>
+                Are you a <RoleLoginHead>TEACHER ?</RoleLoginHead> 
+              </RoleLoginHeading>
+              <RoleLoginInfo>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </RoleLoginInfo>
+              <RoleLoginBtn>Login</RoleLoginBtn>
+            </RoleRightWrapper>
           </RoleLoginRight>
         </RoleLoginTop>
         <RoleLoginBottom>
           <RoleLoginLeft>
-            <RoleLoginHeading>
-              Are you a <span>STUDENT</span> ?
-            </RoleLoginHeading>
-            <RoleLoginInfo>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </RoleLoginInfo>
-            <RoleLoginBtn>Login</RoleLoginBtn>
+            <RoleRightWrapper>
+              <RoleLoginHeading>
+                Are you a <RoleLoginHead>STUDENT ?</RoleLoginHead> 
+              </RoleLoginHeading>
+              <RoleLoginInfo>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </RoleLoginInfo>
+              <RoleLoginBtn>Login</RoleLoginBtn>
+            </RoleRightWrapper>
           </RoleLoginLeft>
           <RoleLoginRight>
             <RoleLoginImage src="https://assets.materialup.com/uploads/a0cc7bb6-d719-44aa-9b5e-7de72d9d6187/preview.jpg" />
