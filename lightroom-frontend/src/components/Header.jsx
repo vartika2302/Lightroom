694949d24bb61dab3593cab7bfb3b5cb44db8817
Vartika <guptavartika624@gmail.com>
2022-06-26 11:34:57 +0530

    Add Header

--- lightroom-frontend/src/components/Header.jsx
+++ lightroom-frontend/src/components/Header.jsx
@@ -1,9 +1,45 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+const HeaderContainer = styled.div``;
+
+const HeaderWrapper = styled.div``;
+
+const HeaderLeft = styled.div``;
+
+const HeaderRight = styled.div``;
+
+const HeaderHeading = styled.h1``;
+
+const HeaderHead = styled.span``;
+
+const HeaderInfo = styled.p``;
+
+const HeaderBtn = styled.button``;
+
+const HeaderImage = styled.img``;
 
 function Header() {
-    return(
-    
-    )
+  return (
+    <HeaderContainer>
+      <HeaderWrapper>
+        <HeaderLeft>
+          <HeaderHeading>
+            <HeaderHead>Online </HeaderHead>
+            Education
+          </HeaderHeading>
+          <HeaderInfo>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </HeaderInfo>
+          <HeaderBtn>Learn more</HeaderBtn>
+        </HeaderLeft>
+        <HeaderRight>
+          <HeaderImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmOUklBd-heAoIOP1OSHSVjwO8wmLATNlmQQ&usqp=CAU" />
+        </HeaderRight>
+      </HeaderWrapper>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
--- lightroom-frontend/src/pages/Home.jsx
+++ lightroom-frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
 
@@ -5,6 +6,7 @@ function Home() {
     return(
         <div className='home'>
             <Navbar/>
+            <Header/>
         </div>
     )
 }
